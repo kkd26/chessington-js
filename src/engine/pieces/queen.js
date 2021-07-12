b36853e165031afdacdec4463a86e00a4122e56d
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-12 12:06:11 +0100

    Add queen movement

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,35 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const { row, col } = currentSquare;
+
+        const availableMoves = [];
+
+        const directions = [
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1],
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, 1],
+        ];
+
+        for (const direction of directions) {
+            for (var i = 1; ; i++) {
+                const newRow = row + i * direction[0];
+                const newCol = col + i * direction[1];
+                const newSquare = Square.at(newRow, newCol);
+
+                if (board.hasSquare(newSquare)) {
+                    availableMoves.push(newSquare);
+                } else {
+                    break;
+                }
+            }
+        }
+        return availableMoves;
     }
 }
