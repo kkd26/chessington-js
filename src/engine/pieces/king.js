e037a23c909cdfd6e2a0973a87cf79ea89a0d02c
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-12 12:14:32 +0100

    Add king movement

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,29 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const { row, col } = currentSquare;
+
+        const availableMoves = [];
+
+        const directions = [
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1],
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, 1],
+        ];
+
+        for (const direction of directions) {
+            const newRow = row + direction[0];
+            const newCol = col + direction[1];
+            const newSquare = Square.at(newRow, newCol);
+
+            if (board.hasSquare(newSquare)) availableMoves.push(newSquare);
+        }
+        return availableMoves;
     }
 }
