16d78a4c27b367a1309e836537df9492f7da5b10
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-12 14:17:23 +0100

    Add logic for taking to queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,41 +1,18 @@
-import Piece from "./piece";
-import Square from "../square";
+import DirectionalPiece from "./directionalPiece";
 
-export default class Queen extends Piece {
+export default class Queen extends DirectionalPiece {
     constructor(player) {
         super(player);
     }
-
-    getAvailableMoves(board) {
-        const currentSquare = board.findPiece(this);
-        const { row, col } = currentSquare;
-
-        const availableMoves = [];
-
-        const directions = [
-            [1, 1],
-            [1, 0],
-            [1, -1],
-            [0, -1],
-            [-1, -1],
-            [-1, 0],
-            [-1, 1],
-            [0, 1],
-        ];
-
-        for (const direction of directions) {
-            for (var i = 1; ; i++) {
-                const newRow = row + i * direction[0];
-                const newCol = col + i * direction[1];
-                const newSquare = Square.at(newRow, newCol);
-
-                if (board.isEmpty(newSquare)) {
-                    availableMoves.push(newSquare);
-                } else {
-                    break;
-                }
-            }
-        }
-        return availableMoves;
-    }
 }
+
+Queen.prototype.directions = [
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+];
